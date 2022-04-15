fcd571379ddaedf88d697d2498f8719bfc306887
myin390 <76754445+myin390@users.noreply.github.com>
2022-04-15 01:05:09 -0400

    inference

---
+++ inference/hp_dicts/tk_vgg16_bn_hp.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# 
+# Author: MIAO YIN
+# Time: 2022/3/6 14:39
+
+class HyperParamsDictRatio2x:
+    ranks = {
+        # 'features.0.weight': [64, 3, 3, 3],
+        'features.3.weight': [32, 64],
+        'features.7.weight': [32, 64],
+        'features.10.weight': [64, 64],
+        'features.14.weight': [64, 128],
+        'features.17.weight': [128, 128],
+        'features.20.weight': [128, 128],
+        'features.24.weight': [128, 128],
+        'features.27.weight': [160, 192],
+        'features.30.weight': [160, 192],
+        'features.34.weight': [160, 192],
+        'features.37.weight': [160, 192],
+        'features.40.weight': [160, 192],
+        'pre_logits.fc1.weight': [256, 256],
+        # 'pre_logits.fc2.weight': [4096, 4096, 1, 1],
+        # 'head.fc.weight': [1000, 4096],
+    }
+
+
+class HyperParamsDictRatio10x:
+    ranks = {
+        'features.3.weight':  [32, 64],
+        'features.7.weight':  [32, 64],
+        'features.10.weight': [32, 64],
+        'features.14.weight': [32, 64],
+        'features.17.weight': [32, 64],
+        'features.20.weight': [64, 64],
+        'features.24.weight': [96, 160],
+        'features.27.weight': [96, 160],
+        'features.30.weight': [96, 192],
+        'features.34.weight': [96, 192],
+        'features.37.weight': [96, 192],
+        'features.40.weight': [96, 192],
+        'pre_logits.fc1.weight': [288, 288],
+        'pre_logits.fc2.weight': [512],
+    }
---
+++ inference/scratch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install tensorly
+pip install timm
+
+mkdir ./saved_models
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1UgP1zFavHGc7Jjre0YyXTxXCBYyJ-aqq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1UgP1zFavHGc7Jjre0YyXTxXCBYyJ-aqq" -O ./saved_models/tkc_resnet18.pt && rm -rf /tmp/cookies.txt
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Jhhrjlvd9byLIb7cUL2aU6d6i8kWOHbz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Jhhrjlvd9byLIb7cUL2aU6d6i8kWOHbz" -O ./saved_models/tkc_resnet50.pt && rm -rf /tmp/cookies.txt
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_zYHW4xE7DUkV54pmPgX0wDYRIwxU9gI' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1_zYHW4xE7DUkV54pmPgX0wDYRIwxU9gI" -O ./saved_models/tkc_vgg16.pt && rm -rf /tmp/cookies.txt
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1u8MXt1Z2XfTdiyABfx-nGwvsCzMxTaMo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1u8MXt1Z2XfTdiyABfx-nGwvsCzMxTaMo" -O ./saved_models/tkc_densenet121.pt && rm -rf /tmp/cookies.txt
