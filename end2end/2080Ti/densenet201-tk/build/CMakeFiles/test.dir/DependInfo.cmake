592ea5f45ea7ae4b49039a31533a8520ca009ae2
lizhi <xianglizhi456@gmail.com>
2022-04-14 23:31:10 -0600

    update

--- end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/DependInfo.cmake
+++ end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -5,29 +5,28 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CUDA
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/Activation.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/Activation.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/Add.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/Add.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/BatchNorm.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/BatchNorm.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/Conv.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/Conv.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/Pool.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/Pool.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/TenDense.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/TenDense.cu.o"
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/main.cu" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/main.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/Activation.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/Activation.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/Add.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/Add.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/BatchNorm.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/BatchNorm.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/Conv.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/Conv.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/Pool.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/Pool.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/TenDense.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/TenDense.cu.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/main.cu" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/main.cu.o"
   )
 set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
 
 # The include file search paths:
 set(CMAKE_CUDA_TARGET_INCLUDE_PATH
-  "/opt/apps/cuda/11.0.3/gcc/6.1.0/targets/x86_64-linux/include"
-  "/home/jiannan.tian/lizhi-temp/cuda/include"
+  "/usr/local/cuda/include"
   )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/src/common.cpp" "/home/jiannan.tian/lizhi-temp/end2end/turker-conv/densenet201-tk/build/CMakeFiles/test.dir/src/common.cpp.o"
+  "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/src/common.cpp" "/home/lizhi/lizhi/TDC-SC22/end2end/2080Ti/densenet201-tk/build/CMakeFiles/test.dir/src/common.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/home/jiannan.tian/lizhi-temp/cuda/include"
+  "/usr/local/cuda/include"
   )
 
 # Targets to which this target links.
